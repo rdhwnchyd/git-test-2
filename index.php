1a0060e064d3ae34425fba66977ff24079946835
rdhwnchyd <ridhwancode11@gmail.com>
2022-04-08 23:55:40 +0700

    menambahkan ul

--- index.php
+++ index.php
@@ -11,5 +11,9 @@
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsam facere perferendis, dicta pariatur eligendi perspiciatis minus illo asperiores aperiam vel labore totam quasi cupiditate consequatur, animi iure nam. Fugiat!
     </p>
+    <ul>
+      <li>satu</li>
+      <li>dua</li>
+    </ul>
   </body>
 </html>
